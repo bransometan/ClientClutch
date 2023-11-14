3d06f3d4b14b1a66a80aaf276fc541aa12df5f5c
brannn99 <bransometan@gmail.com>
2023-11-14 19:18:57 +0800

    Added comments to testing file

--- test_app.py
+++ test_app.py
@@ -9,35 +9,35 @@ def client():
     client = app.test_client()
 
     yield client
-
+# API Testing for Sentiment Analysis
 def test_analyze_sentiment_api(client):
     response = client.post('/api/sentiment', json={'text': 'This is a positive message'})
     assert response.status_code == 200
     data = response.get_json()
     assert 'result' in data
     assert 'recommended_actions' in data
-
+# API Testing for Fraud Analysis
 def test_analyze_fraud_api(client):
     response = client.post('/api/fraud', json={'text': 'This is a potentially fraudulent message'})
     assert response.status_code == 200
     data = response.get_json()
     assert 'result' in data
     assert 'recommended_actions' in data
-
+# API Testing for Security Analysis
 def test_analyze_security_api(client):
     response = client.post('/api/security', json={'text': 'This is a security risk message'})
     assert response.status_code == 200
     data = response.get_json()
     assert 'result' in data
     assert 'recommended_actions' in data
-
+# API Testing for Compliance and Confidentiality Analysis
 def test_analyze_compliance_and_confidentiality_api(client):
     response = client.post('/api/compliconfid', json={'text': 'This is a compliance and confidentiality risk message'})
     assert response.status_code == 200
     data = response.get_json()
     assert 'result' in data
     assert 'recommended_actions' in data
-
+# API Testing for Generate Reply
 def test_generate_reply_api(client):
     response = client.post('/api/generate_reply', json={'message': 'Hello, chatbot!'})
     assert response.status_code == 200
